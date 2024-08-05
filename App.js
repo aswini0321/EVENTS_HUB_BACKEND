6032f09e53364ff20b57ff61f3595282fa3bf4e6
aswini0321 <pedadaaswini2004@gmail.com>
2024-08-05 18:01:00 +0530

    Normalize line endings

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require('cors');
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const app = express();
-const DB_URI="mongodb+srv://lokeswararaodumpala2004:1IaYdSWsAlPSuQW2@cluster0.eurlp56.mongodb.net/Team_Info_DB?retryWrites=true&w=majority";
-mongoose.connect(DB_URI).then(()=>{
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+// app.use(bodyParser.json());
+
+mongoose.connect(process.env.DB_URL).then(()=>{
 console.log("connected to db");
 }).catch((err)=>{
   console.log(err);
@@ -16,52 +19,82 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
-
-const UserSchema =  new mongoose.Schema({
+const StudentSchema =  new mongoose.Schema({
     role:String,
     name:String,
+    collage:String,
     email:String,
     password:String,
-    teamname:String,
-    projectname:String,
-    progress: { type: Number, default: 0 }
-})
- 
 
-const User = new mongoose.model("User",UserSchema);
+})
+
+const Students = new mongoose.model("Students",StudentSchema);
+
+const CompanySchema =  new mongoose.Schema({
+  role:String,
+  name:String,
+  email:String,
+  password:String,
+  appliedEmails: [String]
+})
+const Companies = new mongoose.model("Companies",CompanySchema);
+const posteventSchema = new mongoose.Schema({
+     email:String,
+     dates:String,
+     category:String,
+     description:String
+})
+const Postevents = new mongoose.model("Postevents",posteventSchema);
 
 app.post("/signup",async(req,res)=>{
     try{
-        const {role,name,email,password,teamname,projectname} = req.body;
+        const {role,name,collage,email,password} = req.body;
+         if(role==='Student'){
+        const userExits=await Students.findOne({email:email});
         
-        const userExits=await User.findOne({email:email})
-        console.log(userExits)
         if(userExits)return res.send({success:true,message:"Already user exits"})
      //password hasing ......
         bcrypt.hash(password, saltRounds, async function(err, hash) {
-            const user = await User.create({
+            const user = await Students.create({
                  role:role,
                  name:name,
+                 collage:collage,
                 email:email,
                 password:hash,
-                teamname:teamname,
-                projectname:projectname
+                
             })
             user.save();
             res.send({ success:true,message: 'sucessfully signup' });
-           
-        });
+        });    
+      }
+      else {
+        const userExits=await Companies.findOne({email:email})
+        if(userExits) return res.send({success:true,message:"Already user exits"})
+        bcrypt.hash(password, saltRounds, async function(err, hash) {
+          const user = await Companies.create({
+               role:role,
+               name:name,
+              email:email,
+              password:hash,
+              appliedEmails: []
+          })
+          user.save();
+          res.send({ success:true,message: 'sucessfully signup' });
+        }); 
+      } 
 
-        
     }
     catch(err){
         console.log(err)
     }
 })
+
+
 app.post("/login",async(req,res)=>{
     try {
         const {email,password} = req.body;
-        const userExits = await User.findOne({email:email} )
+        var userExits = await Students.findOne({email:email} )
+        if(!userExits)  userExits = await Companies.findOne({email:email} )
         if(userExits){
             bcrypt.compare(password, userExits.password, function(err, result) {
                 if(result === true) {
@@ -83,86 +116,122 @@ app.post("/login",async(req,res)=>{
         };
 })
 
-app.patch('/updateProgress', async (req, res) => {
-    try {
-      const { _id, progress } = req.body;
-      // Find the user by ID
-      const user = await User.findById(_id);
-      // If the user is found, update the progress; otherwise, send an error
-      if (user) {
-        // If the user doesn't have a progress field, set it to a default value
-        const updatedUser = await User.findOneAndUpdate(
-          { _id },
-          { $set: { progress: progress || 0 } },
-          { new: true } // Return the updated document
-        );
-         
-    // if (!updatedUser) {
-    //     return res.status(404).json({ error: 'User not found' });
-    //   }
-        res.json(updatedUser);
-      } else {
-        res.status(404).send('User not found');
-      }
-    } catch (error) {
-      console.error('Error updating user progress:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  
-  // Define a route to get users by teamname
-app.get('/users/:teamname', async (req, res) => {
-  const { teamname } = req.params;
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = async (to, subject, html) => {
   try {
-    const users = await User.find({ teamname });
-    res.json(users);
+    const info = await transporter.sendMail({
+      from: 'techconnect380@gmail.com',
+      to,
+      subject,
+      html,
+    });
+    console.log('Email sent:', info.messageId);
+    return true;
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error sending email:', error);
+    return false;
+  }
+};
+
+
+app.post("/postevent",async(req,res) => {
+        try{
+       const {   eventDate, category,description, email}  =  req.body;
+       const user = await Postevents.create({
+        email:email,
+        dates:eventDate,
+        category:category,
+        description:description
+   })
+   user.save();
+   const emailSubject = 'New Event Posted!';
+   const emailHtml = `
+     <p>An event has been posted:</p>
+     <p>Date: ${eventDate}</p>
+     <p>Category: ${category}</p>
+     <p>Description: ${description}</p>
+   `;
+   const totalusers=await Students.find({});
+   const userEmails = totalusers.map(student => student.email);
+   const sent = true;
+   if(sent){
+    res.send({ success:true,message: 'sucessfully posted' });
+   } 
+   for (const recipientEmail of userEmails){
+    emailSent = await sendEmail(recipientEmail, emailSubject, emailHtml);
+   }      
+   
+        } catch(err){
+          console.log(err);
+        }
+});
+
+app.get("/allposts",async(req,res)=>{
+  try{
+      const allposts = await Postevents.find()
+      res.send(allposts);
+  } catch(err){
+    console.log(err);
   }
 });
-app.get("/average_progress", async (req,res) => {
-         try{
-          const teamMembers = await User.find({
-            role: { $in: ["TeamMember", "TeamLead"] },
-          });
-      
-          // Use reduce to accumulate team progress data
-          const teamProgressData = teamMembers.reduce((result, user) => {
-            if (!result[user.teamname]) {
-              result[user.teamname] = {
-                teamname: user.teamname,
-                projectname: user.projectname,
-                teamProgress: user.progress,  // Initialize with the user's progress
-                teamMembersCount: 1,  // Start with one member
-              };
-            } else {
-              // If the team is already present, update progress, member count, and projectname
-              result[user.teamname].teamProgress += user.progress;
-              result[user.teamname].teamMembersCount += 1;
-              result[user.teamname].projectname = user.projectname;
-            }
-      
-            return result;
-          }, {});
-      
-          // Calculate the average progress for each team and object.values is to convert object into array
-          const averageTeamProgress = Object.values(teamProgressData).map((team) => ({
-            teamname: team.teamname,
-            projectname: team.projectname,
-            averageProgress: team.teamMembersCount
-              ? (team.teamProgress / team.teamMembersCount).toFixed(0)
-              : 0,
-          }));
-      
-          // Send the average team progress data as a JSON response
-          res.json(averageTeamProgress);
-         } catch(error){
-          console.log(error);
-         }
-} );
 
+app.patch("/applied",async(req,res)=>{
+    const {Semail , Cemail} = req.body;
+    try {
+      const companyExists = await Companies.findOne({ email: Cemail });
+      if (!companyExists) {
+          return res.status(404).json({ message: "Company not found" });
+      }
+
+      if (companyExists.appliedEmails.includes(Semail)) {
+                return res.status(200).json({ message: "Already you applied" });
+            }
+            // If the email is not already present, push it to the appliedEmails array
+        companyExists.appliedEmails.push(Semail);
+        await companyExists.save();
+      return res.status(200).json({  message: "Succesfully Applied" });
+  } catch (error) {
+      console.error("Error updating appliedEmails:", error);
+      return res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+app.get("/company/:email",async(req,res)=>{
+    try{  
+  const {email} = req.params;
+    const company =  await Companies.findOne({ email: email });
+    if (!company) {
+      return res.status(404).json({ success: false, message: "Company not found" });
+  }
+  let appliedStudentsData = [];
+  for (const email of company.appliedEmails) {
+    const student = await Students.findOne({ email: email });
+    if (student) {
+        // Extract required fields and push to studentsData array
+        appliedStudentsData.push({
+            name: student.name,
+            email: student.email,
+            collage: student.collage
+        });
+        } 
+     }
+     res.status(200).json({students: appliedStudentsData });
+    } catch(err){
+      console.log(err);
+      res.status(500).json({ success: false, message: "Internal server error" });
+    }
+                
+});
 
 app.listen(4000,function(){
   
